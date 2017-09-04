d5f0ef6a378fe6f75bf680d1323e97b71a5822e6
James Sullivan <jamesfsullivan5@gmail.com>
2017-09-03 18:44:15 -0600

    Add finished basic socket chat app.

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    console.log('Message: ', msg);
+    io.emit('chat message', msg);
+  })
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
