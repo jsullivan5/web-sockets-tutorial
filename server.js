978f99acfc84101737fece990f61c81525840b48
James Sullivan <jamesfsullivan5@gmail.com>
2017-09-07 19:37:25 -0600

    Add missing semicolons

--- server.js
+++ server.js
@@ -7,18 +7,18 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  io.emit('initial', 'A user has connected')
+  io.emit('initial', 'A user has connected');
 
   socket.on('new message', (msg) => {
     socket.broadcast.emit('new message', msg);
   });
 
   socket.on('disconnect', function(){
-    io.emit('end', 'A User has disconnected')
+    io.emit('end', 'A User has disconnected');
   });
 
   socket.on('typing', function(userName) {
-      socket.broadcast.emit('typing', `${userName} is typing.`)
+      socket.broadcast.emit('typing', `${userName} is typing.`);
   });
 });
 
